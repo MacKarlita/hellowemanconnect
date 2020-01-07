e41aeace048f78d3b46d1adb7f633312626009f5
Karla Martin <karla.martin@unosquare.com>
2020-01-07 15:49:01 -0600

    Revert "Some scripts"
    This reverts commit 46f50c40ad1d3b64060121a9e61c18286095c213.

--- hello_bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Print Welcome message
-echo "===Hello Bash==="
-
-# Read input using read command
-read -p "May I know your name, please? " name
-echo "Hello $name, Welcome to WemanConnect Bootcamp!"
