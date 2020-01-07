46f50c40ad1d3b64060121a9e61c18286095c213
Karla Martin <karla.martin@unosquare.com>
2020-01-07 15:46:40 -0600

    Some scripts

---
+++ hello_bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Print Welcome message
+echo "===Hello Bash==="
+
+# Read input using read command
+read -p "May I know your name, please? " name
+echo "Hello $name, Welcome to WemanConnect Bootcamp!"
